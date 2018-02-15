a4c425756d0be7276a2eef8565474e4e2dfa8a6b
Evandro Matias <evandromattias@gmail.com>
2018-02-15 09:48:50 -0200

    add repository

---
+++ server/src/repositories/horary-repository.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const Horary = mongoose.model("Horary")
+
+exports.get = async (id) => {
+  const res = await Horary
+    .find({ user: id })
+    .populate("user")
+  return res
+}
+
+exports.getById = async (id) => {
+  const res = await Horary.getById(id)
+    .populate("user")
+  return res
+}
+
+exports.create = async (data) => {
+  const horary = new Horary(data)
+  await horary.save()
+}
+
+exports.update = async (id, data) => {
+  await Horary.findByIdAndUpdate(id, {
+    $set: {
+      day: data.day,
+      start: data.start,
+      startLunch: data.startLunch,
+      endLunch: data.endLunch,
+      end: data.end
+    }
+  })
+}
+
+exports.remove = async(id) => {
+  await Horary.findOneAndRemove(id)
+}
